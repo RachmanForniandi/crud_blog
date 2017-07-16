c97356b4edc6ff6b728cce832dc9732aa9f95762
RachmanForniandi <rachmanforniandi@gmail.com>
2017-07-16 18:41:01 +0700

    template html dan css

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+require_once "view/header.php";
+?>
+
+<div class="tiap_artikel">
+	<h3>Judul artikel</h3>
+    <p>
+    	Isi artikel
+    </p>
+    <p class="waktu">12:00 AM</p>
+    <p class="tag">Sepakbola Inggris</p>
+</div>
+
+<div class="tiap_artikel">
+	<h3>Judul artikel</h3>
+    <p>
+    	Isi artikel
+    </p>
+    <p class="waktu">12:00 AM</p>
+    <p class="tag">Sepakbola Spanyol</p>
+</div>
+
+
+<?php 
+require_once "view/footer.php";
+?>
---
+++ view/header.php
@@ -0,0 +1,6 @@
+<head>
+	<title>Soccer News</title>
+    <link rel="stylesheet" href="view/style.css">
+</head>
+
+<h1>Blog Soccer News</h1>
